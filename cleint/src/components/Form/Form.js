56e0b896ada33e7707ddd41eaf41f95158d0ba21
Сергей <nat29011985@gmail.com>
2021-11-12 08:37:10 +0300

    correction form

--- cleint/src/components/Form/Form.js
+++ cleint/src/components/Form/Form.js
@@ -1,22 +1,22 @@
 
 
-function Form({handlerForm}) {
+function Form({ handlerForm }) {
 
   return (
     <div className="card mt-3" >
-      <form onSubmit={(e)=>handlerForm(e)}>
+      <form onSubmit={(e) => handlerForm(e)}>
         <div className="m-3">
           <label htmlFor="nickname" className="form-label">Nickname</label>
-          <input type="text" className="form-control" id="nickname" name="nickname" aria-describedby="emailHelp"  />
+          <input type="text" className="form-control" id="nickname" name="nickname" aria-describedby="emailHelp" required />
         </div>
         <div className="m-3">
           <label htmlFor="message" className="form-label">Your message</label>
-          <textarea type="text" className="form-control" id="message" name="message" aria-describedby="emailHelp"  />
+          <textarea type="text" className="form-control" id="message" name="message" aria-describedby="emailHelp" required />
         </div>
-        <button type="submit" className="btn btn-primary m-3">Submit</button>
+        <button type="submit" className="btn btn-primary m-3">leave a message</button>
       </form>
     </div>
   )
 }
 export default Form
-//required
+//required .env.sample
